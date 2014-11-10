ae94ce45697c8f01e0dfe71c68130c545d271235
vencendor <123qwer>
2014-11-11 01:54:07 +0200

    Revert "Yboard версия 2 из"
    This reverts commit 4ebc989678f597d1abc0a0e5739cc5e96063200c.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tar --transform='s,^,yboard/,s'  \
---exclude ".hg*" \
---exclude nbproject \
---exclude deploy.sh \
---exclude "yboard.rev*.tar.gz" \
--czf yboard.rev`hg log --rev . --template "{rev}"`.tar.gz * .htaccess
--- themes/yboard/views/admin/layouts/column1.php
+++ themes/yboard/views/admin/layouts/column1.php
@@ -1,5 +1,6 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('/layouts/main'); ?>
+ADmin
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
