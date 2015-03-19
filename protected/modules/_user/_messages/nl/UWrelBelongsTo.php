454c572c11f38508b7bf1dfebfff07249c642b00
vencendor <123qwer>
2015-03-19 21:22:18 +0200

    После Очистки

--- protected/modules/_user/_messages/nl/UWrelBelongsTo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-	'Model Name'=>'Modelnaam',
-	'Lable field name'=>'Lable veldnamen',
-	'Empty item name'=>'Leeg item-naam',
-	'Profile model relation name'=>'Profiel model relatienaam',
-);
