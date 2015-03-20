3f5582bd91bc470f360a485d2ee07cfab74f42a5
vencendor <123qwer>
2015-03-21 00:35:43 +0200

    config

--- protected/config/main_dev.php
+++ protected/config/main_dev.php
@@ -32,32 +32,6 @@ return array(
         // If removed, Gii defaults to localhost only. Edit carefully to taste.
         //'ipFilters' => array('127.0.0.1', '::1', '192.168.1.3'),
         ),
-        /**/
-        /*
-          'user' => array(
-          # encrypting method (php hash function)
-          'hash' => 'md5',
-          # send activation email
-          'sendActivationMail' => true,
-          # allow access for non-activated users
-          'loginNotActiv' => false,
-          # activate user on registration (only sendActivationMail = false)
-          'activeAfterRegister' => false,
-          # automatically login from registration
-          'autoLogin' => true,
-          # registration path
-          'registrationUrl' => array('/user/registration'),
-          # recovery password path
-          'recoveryUrl' => array('/user/recovery'),
-          # login form path
-          'loginUrl' => array('/user/login'),
-          # page after login
-          'returnUrl' => array('/user/profile'),
-          # page after logout
-          'returnLogoutUrl' => array('/user/login'),
-          ),
-         * 
-         */
         'admin',
     ),
     // application components
